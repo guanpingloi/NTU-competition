2010dd5ca2fd85954bce389355ff8ad5abdd07ac
charliemingloi <51398428+charliemingloi@users.noreply.github.com>
2022-08-12 10:35:05 +0800

    First version

---
+++ libraries/template.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#include <iostream>
+
+typedef long long ll;
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    string name, first_name;
+    cout << "Enter your name: ";
+    getline(cin, name);
+    cout << "Hello " << name << endl;
+    cin >> first_name;
+    cout << "First Name: " << first_name; 
+}
